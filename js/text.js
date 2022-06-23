ebdda6fa5ca3cca68473d8ced007d56219435864
antonin <99360974+ZaYtI@users.noreply.github.com>
2022-06-24 00:24:12 +0200

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,16 @@
+function OnClickDiv() {
+    window.location.href="https://www.google.com";
+
+}
+
+function OnClickFacebook(){
+    window.location.href="https://m.facebook.com/EMS-Location-101000738888860/posts"
+}
+
+function OnClickTwitter(){
+    window.location.href=""
+}
+
+function OnClickInstagram(){
+    window.location.href=""
+}
